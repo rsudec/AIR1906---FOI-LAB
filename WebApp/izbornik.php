b50a28e77ffc93e865248943567b5e92aefbcdbf
irenailisevic <iilisevic@foi.hr>
2019-11-24 15:23:26 +0100

    6.4. Izrada dizajna sučelja izbornika - web
    Dizajn sučelja izbornika. Kreirana odgovarajuća css datoteka s pravilima za dizajn glavnog izbornika za web aplikaciju (webapp.css).  Kreirana .php datoteka sa sadržajem izbornika (izbornik.php).

---
+++ WebApp/izbornik.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+<html>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Web-app</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/webapp.css">
+
+</head>
+<body>
+
+<ul>
+    <li><a href="">Obavijesti</a></li>
+    <li><a href="">Dnevnik</a></li>
+    <li><a href="">Resursi</a></li>
+    <li><a href="">Novi resurs</a></li>
+    <li><a href="">Novi korisnik</a></li>
+    <li><a href="index.php">Početna</a></li>
+    <li class="odjava"><a class="aodjava" href="">Odjava</a></li>
+</ul>
+</html>
