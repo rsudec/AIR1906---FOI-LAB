c6bd6a2496609bcfadb157d421b27e630b13bcd5
irenailisevic <iilisevic@foi.hr>
2019-11-24 17:06:49 +0100

    5.4. Izrada dizajna sučelja prijave - web
    Dodana datoteka prijava.php i datoteka header.php. Dopunjena css datoteka pravilima za dizajn obrasca prijave. U datoteku prijava.php dodana potrebna polja za prijavu na koja su primjenjena pravila dizajna.

---
+++ WebApp/prijava.php
@@ -0,0 +1,20 @@
+<html>
+<body class="prijavabgc">
+<br>
+<br>
+<br>
+        <div class="prijavafrm">
+<h2>Prijava</h2>
+        <form action='' method='POST' id='prijava'>
+
+            <input type='text' class="inputprijava" id='email' name='email' placeholder="Korisničko ime" required autofocus /><br />
+<br>
+            <input type='password' class="inputprijava" id='lozinka' name='lozinka' placeholder="Lozinka" required /><br />
+<br>
+            <input class="inputprijava" id="gumbprijava" type='submit'name="prijava_gumb" value='Prijava' />
+        </form>
+        </div>
+</body>
+</html>
+<?php
+include_once 'header.php';
