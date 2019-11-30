9dc7594164ddfc99b5ada95727ec49f31513f650
rsudec <rsudec@foi.hr>
2019-11-30 23:07:26 +0100

    početni screen za sada je registerScreen

--- MobileApp/air1906_flutter/lib/main.dart
+++ MobileApp/air1906_flutter/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'screens/register_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(body: Center(child: Text('AIR 1906'),),),
+      home: RegisterScreen(),
     );
   }
 }
