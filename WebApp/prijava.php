9fb4f78715e385dc0e25eb9ed3a7544f9cccf649
irenailisevic <iilisevic@foi.hr>
2020-01-09 21:20:19 +0100

    #19
    Dorada prijava.php u skladu s promjenom na api-ju za slanje podataka post metodom

--- WebApp/prijava.php
+++ WebApp/prijava.php
@@ -1,4 +1,5 @@
 <html>
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <body class="prijavabgc">
 <br>
 <br>
@@ -12,6 +13,8 @@
             <input type='password' class="inputprijava" id='lozinka' name='lozinka' placeholder="Lozinka" required /><br />
 <br>
             <input class="inputprijava" id="gumbprijava" type='submit' name="prijava_gumb" value='Prijava' />
+
+            <input type="hidden" id="_token" value="{{ csrf_token() }}">
         </form>
 </div>
 <div id="test"></div>
@@ -45,9 +48,44 @@ if (isset($_POST['prijava_gumb'])) {
     $korime = $_POST["korime"];
     $lozinka = $_POST["lozinka"];
 
+/*
     $url = "https://air-api.azurewebsites.net/Prijava/$korime/$lozinka";
     $data = file_get_contents($url);
     $korisnik = json_decode($data);
+*/
+
+    $data = array(
+        'korime'      => $korime,
+        'lozinka'    => $lozinka,
+        '_token'=> '{{csrf_token()}}'
+
+    );
+
+    $url = "https://air-api.azurewebsites.net/Prijava";
+    $content = json_encode($data);
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER,
+        array("Content-type: application/json"));
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+
+    $json_odgovor = curl_exec($curl);
+
+    $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+    if ( $status != 200 ) {
+        die("Error: call to URL $url failed with status $status, response $json_odgovor, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+    }
+
+
+    curl_close($curl);
+
+    $korisnik = json_decode($json_odgovor);
+
+
     if ($korisnik==null){
         echo '<script language="javascript">';
         echo 'alert("Pogrešno korisničko ime ili lozinka!")';
