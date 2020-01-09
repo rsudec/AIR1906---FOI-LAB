7d04d32f95b04c6929454bf010d0ef4a304319bf
leteca_svinja <magichnipuding@gmail.com>
2020-01-09 20:43:18 +0100

    Adaptacija kontrolera i funkcija, dodavanje putanja

--- API/routes/web.php
+++ API/routes/web.php
@@ -30,6 +30,6 @@ Route::get('/IspisLog/{id_kor?}','LogController@IspisLog');
 Route::get('/IspisObavijesti','LogController@IspisObavijesti');
 Route::get('/IspisNeprocitanih','LogController@IspisNeprocitanih');
 
-Route::get('/Prijava/{korime}/{lozinka}','KorisnikController@Prijava');
+Route::post('/Prijava','KorisnikController@Prijava');
 Route::get('/SviKorisnici','KorisnikController@SviKorisnici');
-Route::get('/DodajKorisnika/{ime}/{prezime}/{telefon}/{adresa}/{email}/{oib}/{korime}/{lozinka}/{poslovnica}','KorisnikController@DodajKorisnika');
+Route::post('/DodajKorisnika','KorisnikController@DodajKorisnika');
