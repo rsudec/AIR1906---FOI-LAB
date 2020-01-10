79bb2b17f6e56a3c02e2c5ac5a62a2acc9600bb5
irenailisevic <iilisevic@foi.hr>
2020-01-10 12:49:57 +0100

    #35
    Implementacija funkcionalnosti dodavanje novog korisnika novi_korisnik.php, dadane provjere zauzetosti e-mail adrese i korisničkog imena, dodana provjera unesene lozinke i ponovljene lozinke (registracija.js). Potrebna dorada za provjeru je li korisnik doista dodan u bazu podataka i dorada dodavanja poslovnice u vidu padajućeg izbornika.

---
+++ WebApp/javascript/registracija.js
@@ -0,0 +1,81 @@
+$(document).ready(function($) {
+    $(document).on('input', '#ponovljena_lozinka', function() {
+
+        var v = $("#lozinka").val();
+        var v2 = $("#ponovljena_lozinka").val();
+        var greska=$("#greska");
+
+        if (v === v2)
+        {
+            greska.text(" ");
+            $("#gumbregistracija").prop("disabled", false);
+
+        } else {
+
+            greska.text("Lozinka i ponovljena lozinka nisu jednake");
+            greska.css('color', 'red');
+            $("#gumbregistracija").prop("disabled", true);
+        }
+    });
+
+    $(document).on('input', '#email', function() {
+
+        var email = $("#email").val();
+
+        var greska=$("#greska1");
+
+
+        $.ajax({
+            type: "GET",
+            url: "https://air-api.azurewebsites.net/ShowUsers",
+            success: function (result) {
+
+                for (var i in result) {
+                    if (result[i].email===email){
+                        greska.text("Već postoji korisnik s unesenom e-mail adresom");
+                        greska.css('color', 'red');
+                        $("#gumbregistracija").prop("disabled", true);
+                    }
+                    else {
+                        greska.text(" ");
+                        $("#gumbregistracija").prop("disabled", false);
+                    }
+
+                }
+
+            }
+        });
+    });
+
+        $(document).on('input', '#korime', function() {
+
+            var korime = $("#korime").val();
+
+            var greska=$("#greska2");
+
+
+            $.ajax({
+                type: "GET",
+                url: "https://air-api.azurewebsites.net/ShowUsers",
+                success: function (result) {
+
+                    for (var i in result) {
+                        if (result[i].kor_ime===korime){
+                            greska.text("Korisničko ime je zauzeto");
+                            greska.css('color', 'red');
+                            $("#gumbregistracija").prop("disabled", true);
+                        }
+                        else {
+                            greska.text(" ");
+                            $("#gumbregistracija").prop("disabled", false);
+                        }
+
+                    }
+
+                }
+            });
+
+    });
+
+
+});
--- WebApp/novi_korisnik.php
+++ WebApp/novi_korisnik.php
@@ -1,40 +1,130 @@
 <html>
 <head>
     <title>Novi korisnik</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <script src="javascript/registracija.js"></script>
 </head>
 <body class="novi_korisnik">
 <?php
 include_once 'izbornik.php';
 include_once 'header.php';
+/*$prijava = dohvatiLogKorId();
+
+if ($prijava == null) {
+    header("Location: prijava.php");
+}*/
 ?>
     <br>
 <div class="reg">
     <br>
     <h2 id="titleKorisnik">Novi korisnik</h2>
-    <form action="" method=\'POST\' id=\'dodaj_korisnika\' class="registracija">
+    <form action="novi_korisnik.php" method='POST' id='dodaj_korisnika' class="registracija">
             <label for="ime">Ime</label><br>
-            <input type=\'text\' class="noviKorisnik" id=\'ime\' name=\'ime\' required /><br />
+            <input type='text' class="noviKorisnik" id='ime' name='ime' required /><br />
             <label for="prezime">Prezime</label><br>
-            <input type=\'text\' class="noviKorisnik" id=\'prezime\' name=\'prezime\' required /><br />
-            <label for="datum">Datum rođenja</label><br>
-            <input type="date" class="noviKorisnik" id=\'datum\' name=\'datum\'  required /><br />
+            <input type='text' class="noviKorisnik" id='prezime' name='prezime' required /><br />
+
             <label for="telefon">Telefon</label><br>
-            <input type=\'text\' class="noviKorisnik" id=\'telefon\' name=\'telefon\'  required /><br />
+            <input type='text' class="noviKorisnik" id='telefon' name='telefon'  required /><br />
             <label for="adresa">Adresa</label><br>
-            <input type=\'text\' class="noviKorisnik" id=\'adresa\' name=\'adresa\' required /><br />
+            <input type='text' class="noviKorisnik" id='adresa' name='adresa' required /><br />
             <label for="email">Email</label><br>
-            <input type=\'text\' class="noviKorisnik" id=\'email\' name=\'email\' required /><br />
+            <input type='text' class="noviKorisnik" id='email' name='email' required /><div id="greska1"></div><br />
             <label for="oib">OIB</label><br>
-            <input type=\'text\' class="noviKorisnik" id=\'oib\' name=\'oib\' required /><br />
+            <input type='text' class="noviKorisnik" id='oib' name='oib' required /><br />
             <label for="korime">Korisničko ime</label><br>
-            <input type=\'text\' class="noviKorisnik" id=\'korime\' name=\'korime\' required /><br />
+            <input type='text' class="noviKorisnik" id='korime' name='korime' required /><div id="greska2"></div><br />
             <label for="lozinka">Lozinka</label><br>
-            <input type=\'text\' class="noviKorisnik" id=\'lozinka\' name=\'lozinka\' required /><br />
+            <input type='password' class="noviKorisnik" id='lozinka' name='lozinka' required /><br />
+            <label for="ponovljena_lozinka">Ponovljena lozinka</label><br>
+            <input type='password' class="noviKorisnik" id='ponovljena_lozinka' name='ponovljena_lozinka' required /><br />
+        <div id="greska"></div><br>
             <br>
-            <input class="inputKorisnik" id="gumbregistracija" type='submit'name="registracija_gumb" value='Registracija'/>
+            <input class="inputKorisnik" id="gumbregistracija" type='submit' name="registracija_gumb" value='Registracija'/>
+
+            <input type="hidden" id="_token" value="{{ csrf_token() }}">
             <br>
             <br>
     </form>
 </div>
 </body>
 </html>
+<?php
+if (isset($_POST['registracija_gumb'])) {
+
+    $ime = $_POST["ime"];
+    $prezime = $_POST["prezime"];
+
+    $telefon = $_POST["telefon"];
+    $adresa = $_POST["adresa"];
+    $email = $_POST["email"];
+    $oib = $_POST["oib"];
+    $korime = $_POST["korime"];
+    $lozinka = $_POST["lozinka"];
+
+    $url = "https://air-api.azurewebsites.net/SviKorisnici";
+    $data = file_get_contents($url);
+    $podaci = json_decode($data);
+
+    $ispravno=true;
+
+    foreach ($podaci as $podatak){
+        if ($podatak->email==$email){
+            echo '<script language="javascript">';
+            echo 'alert("Već postoji korisnik s unesenom e-mail adresom")';
+            echo '</script>';
+            $ispravno=false;
+        }
+        else if ($podatak->kor_ime==$korime){
+            echo '<script language="javascript">';
+            echo 'alert("Korisničko ime je zauzeto")';
+            echo '</script>';
+            $ispravno=false;
+        }
+    }
+
+    if ($ispravno){
+        $data = array(
+            'ime'      => $ime,
+            'prezime'    => $prezime,
+            'telefon'    => $telefon,
+            'adresa'    => $adresa,
+            'email'    => $email,
+            'oib'    => $oib,
+            'korime'    => $korime,
+            'lozinka'    => $lozinka,
+            'poslovnica'    => 1,
+            '_token'=> '{{csrf_token()}}'
+
+        );
+
+        $url = "https://air-api.azurewebsites.net/DodajKorisnika";
+        $content = json_encode($data);
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER,
+            array("Content-type: application/json"));
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+
+        $json_odgovor = curl_exec($curl);
+
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        if ( $status != 200 ) {
+            die("Error: call to URL $url failed with status $status, response $json_odgovor, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+        }
+
+
+        curl_close($curl);
+
+        $korisnik = json_decode($json_odgovor);
+
+        if ($korisnik==null){
+            echo 'nije dobro';
+        }
+    }
+}
