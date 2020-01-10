cc320a16f6d528ea9fdbfac3aea8815046f436d9
isiser <i.siser02@gmail.com>
2020-01-10 14:44:28 +0100

    Dovršen izgled myResourceScreen-a

--- MobileApp/air1906_flutter/lib/screens/myResourcesScreen.dart
+++ MobileApp/air1906_flutter/lib/screens/myResourcesScreen.dart
@@ -1,9 +1,85 @@
 import 'package:flutter/material.dart';
+import '../widgets/MyResourceItem.dart';
+import '../models/Resource.dart';
+import '../viewModel/CategoriesViewModel.dart';
 
 class MyResourcesScreen extends StatelessWidget {
   static const routeName = "/myResources";
+  final CategoriesViewModel _categoriesViewModel = CategoriesViewModel();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 15, left: 10, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    color: Colors.white,
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  Expanded(
+                    child: Text(
+                      'Popis mojih resursa',
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  IconButton(
+                    color: Colors.black,
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: null,
+                  ),
+                ],
+              ),
+            ),
+            Stack(
+              children: <Widget>[
+                Container(color: Colors.white),
+                Container(
+                  height: MediaQuery.of(context).size.height - 85,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(
+                        35,
+                      ),
+                      topRight: Radius.circular(
+                        35,
+                      ),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: StreamBuilder(
+                      stream: _categoriesViewModel.observableCategoryList,
+                      builder: (context, streamsnapshot) {
+                        return ListView.builder(
+                          itemCount: 10,
+                          itemBuilder: (ctx, i) => MyResourceItem(Resource(
+                              i.toString(),
+                              "Test $i",
+                              0,
+                              "https://images.idgesg.net/images/article/2019/10/aceraspire1-100815704-large.jpg",
+                              Duration(days: 2))),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ MobileApp/air1906_flutter/lib/widgets/MyResourceItem.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import '../models/Resource.dart';
+import '../screens/ResourceDetailScreen.dart';
+
+class MyResourceItem extends StatelessWidget {
+  final Resource resource;
+  MyResourceItem(this.resource);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context)
+            .pushNamed(ResourceDetailScreen.routeName, arguments: resource);
+      },
+      child: Hero(
+          tag: resource.id,
+          child: Card(
+            child: ListTile(
+              title: Text(resource.naziv),
+              leading:
+                  CircleAvatar(backgroundImage: NetworkImage(resource.imgUrl)),
+              subtitle: Text(
+                  "Posudba istiće ${resource.maxVrijemePosudbe.inDays} dana"),
+              trailing: Container(
+                  width: 100,
+                  child: CircleAvatar(
+                    backgroundColor: resource.kolicina > 0
+                        ? Colors.green[300]
+                        : Colors.red[300],
+                    child: Text(
+                      resource.kolicina.toString(),
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )),
+            ),
+          )
+          // child: ListTile(
+          //   title: Text(resource.naziv),
+          //   leading: CircleAvatar(backgroundImage: NetworkImage(resource.imgUrl)),
+          //   subtitle:
+          //       Text("Posudba traje ${resource.maxVrijemePosudbe.inDays} dana"),
+          //   trailing: Container(
+          //       width: 100,
+          //       child: CircleAvatar(
+          //         backgroundColor:
+          //             resource.kolicina > 0 ? Colors.green[300] : Colors.red[300],
+          //         child: Text(
+          //           resource.kolicina.toString(),
+          //           style: TextStyle(color: Colors.white),
+          //         ),
+          //       )),
+          // ),
+          ),
+    );
+  }
+}
