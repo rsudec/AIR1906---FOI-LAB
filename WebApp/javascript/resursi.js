dd36850b0fab0fcabc882f0ffa66cbb094c658ce
irenailisevic <iilisevic@foi.hr>
2020-01-12 14:58:17 +0100

    #67
    Dodana datoteka resursi.js za dohvaćanje podataka o resursima s api-ja, dodan odgovarajući kod za prikaz podataka o resursima na ekranu (resursi.php)

---
+++ WebApp/javascript/resursi.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+    var prikazDnevnik = $("#prikaz_resursi");
+
+    prikazDnevnik.text("Učitavanje podataka...");
+    dohvatiPodatke();
+    $.ajaxSetup ({
+        cache: false
+    });
+
+    function dohvatiPodatke() {
+
+        $.ajax({
+            type: "GET",
+            url: "https://air-api.azurewebsites.net/SviResursi",
+            success: function (result) {
+                var output =
+                    "<table id='tablica'><thead><tr><th>ID resursa</th><th>Naziv</th><th>Tip resursa</th><th>Količina</th><th>Zauzeto</th>" +
+                    "<th>Maksimalno vrijeme<br> posudbe (u danima)</th><th>Slika</th></thead><tbody>";
+
+                for (var i in result) {
+                    var slika=result[i].slika;
+                    output +=
+                        "<tr><td>" +
+                        result[i].id_resurs +
+                        "</td><td>" +
+                        result[i].nazivr +
+                        "</td><td>" +
+                        result[i].nazivtr +
+                        "</td><td>" +
+                        result[i].kolicina+
+                        "</td><td>" +
+                        result[i].zauzeto+
+                        "</td><td>" +
+                        result[i].max_posudba +
+                        "</td><td>" +
+                        "<img src='"+slika+"'>"+
+                        "</td></tr>";
+                }
+                output += "</tbody></table>";
+
+                prikazDnevnik.html(output);
+
+                $('table').DataTable(
+                    {
+
+                    });
+                setTimeout(dohvatiPodatke, 10000);
+            }
+        });
+    }
+
+
+});
--- WebApp/resursi.php
+++ WebApp/resursi.php
@@ -1,8 +1,17 @@
 <html>
 <head>
     <title>Resursi</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="javascript/resursi.js"></script>
 </head>
 </html>
 <?php
 include_once 'izbornik.php';
-echo '<body><h1>Resursi</h1></body>';
+?>
+<html>
+<body><h1>Resursi</h1></body>
+<div id="prikaz_resursi"></div>
+</html>
