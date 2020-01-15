d9141b4b377ef68d1b862e2f26e0cd8a1aed15ae
rsudec <rsudec@foi.hr>
2020-01-15 01:59:37 +0100

    testiranje NFC recommit

---
+++ MobileApp/air1906_flutter/lib/widgets/NFC.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:nfc_manager/nfc_manager.dart';
+import '../service/ResourceService.dart';
+
+enum NFCType { borrowResource, returnResource }
+
+class NFC extends StatefulWidget {
+  final NFCType type;
+  // final resourceId;
+  NFC(this.type);
+  @override
+  _NFCState createState() => _NFCState();
+}
+
+class _NFCState extends State<NFC> {
+  var success = false;
+  var result = "";
+  @override
+  void initState() {
+    super.initState();
+    listenNFC();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    NfcManager.instance.stopSession(
+      alertMessageIOS: '...',
+      errorMessageIOS: '...',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var type = widget.type;
+    return Center(
+      child: Container(
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(
+              type == NFCType.borrowResource ? "Posudi resurs" : "Vrati resurs",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Text("Prislonite uređaj na NFC oznaku"),
+            Container(
+              child: Image(
+                image: AssetImage("assets/images/NFCImage.png"),
+              ),
+            ),
+            Text(result),
+            success
+                ? Text(
+                    result,
+                    style: TextStyle(
+                        color: Colors.green, fontWeight: FontWeight.bold),
+                  )
+                : CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void listenNFC() {
+    NfcManager.instance.startTagSession(
+      alertMessageIOS: '...',
+      pollingOptions: {
+        TagPollingOption.iso14443,
+        TagPollingOption.iso15693,
+        TagPollingOption.iso18092
+      },
+      onDiscovered: (NfcTag tag) async {
+        Ndef ndef = Ndef.fromTag(tag);
+        if (ndef == null) {
+          print("tag is not ndef");
+          return;
+        }
+        NdefRecord data = ndef.cachedMessage.records.first;
+        String resourceIdFromNFC = String.fromCharCodes(data.payload);
+
+        ResourceService rs = ResourceService();
+        if (widget.type == NFCType.borrowResource) {
+          var response = await rs.borrowResource(resourceIdFromNFC).then((val) {
+            setState(() {
+              print("res data ${val.data}");
+              if (val.data) {
+                success = true;
+                result = "Uspješno ste posudili $resourceIdFromNFC";
+              } else {
+                success = false;
+              }
+            });
+          });
+        }
+        else if(widget.type == NFCType.returnResource){
+          var response = await rs.returnResource(resourceIdFromNFC).then((val) {
+            setState(() {
+              print("res data ${val.data}");
+              if (val.data) {
+                success = true;
+                result = "Uspješno ste vratili $resourceIdFromNFC";
+              } else {
+                success = false;
+              }
+            });
+          });
+        }
+      },
+    );
+  }
+}
